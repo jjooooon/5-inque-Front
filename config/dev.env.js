48a778292eaaa14185fc5fdbf1454973d5b0b726
Jeong Jae Ha <jaeha2324@gmail.com>
2023-04-13 20:03:59 +0900

    feat: add argocd file

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development1"',
   VUE_APP_WAS_LOCAL: '"http://localhost:8085"',
-  VUE_APP_WAS: '"http://aeeefee366f824c9d96dd709aa06d671-355570676.ap-northeast-2.elb.amazonaws.com:8085"'
+  VUE_APP_WAS: '"http://10.100.175.238:8085"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  VUE_APP_WAS: '"http://aeeefee366f824c9d96dd709aa06d671-355570676.ap-northeast-2.elb.amazonaws.com"'
+  VUE_APP_WAS: '"http://10.100.175.238:8085"'
 }
